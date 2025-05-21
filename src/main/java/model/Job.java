245659a782ddc9fa71bf1849079fb6de132095ab
Chris Miller <chris428miller@gmail.com>
2025-05-21 18:02:28 -0400

    Create Job.java model class

---
+++ src/main/java/model/Job.java
@@ -0,0 +1,58 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "job")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+    private long id;
+    private String position;
+    private String company;
+    private String status;
+    private String notes;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    
+}
