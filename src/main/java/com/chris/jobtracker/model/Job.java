57680d8e72ee00ca6d88a66dfa813a210a1e549a
Chris Miller <chris428miller@gmail.com>
2025-05-27 22:49:15 -0400

    Update Job.java

--- src/main/java/com/chris/jobtracker/model/Job.java
+++ src/main/java/com/chris/jobtracker/model/Job.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Setter; 
 
 @Entity
 @Getter
